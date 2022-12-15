615a9fef714a7a9ad97a5074dd14f34280308ac7
Nevin Zheng <nevzheng@gmail.com>
2022-12-14 22:28:38 -0800

    Solve: 15

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -113,6 +113,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 15);
-        assert_eq!(part_two(&input), Some(56000011));
+        assert_eq!(part_two(&input), None);
     }
 }
